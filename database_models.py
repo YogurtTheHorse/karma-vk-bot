3eef2829ae44a202352728ba8a9d5b3c03d72831
yegorf1 <yegorf1@gmail.com>
2018-07-30 14:01:59 +0300

    Add required methods and fields for simple implementation

--- database_models.py
+++ database_models.py
@@ -1,10 +1,22 @@
 from collections import OrderedDict
 
-from mongoengine import Document, IntField, ReferenceField, StringField, DoesNotExist
+import datetime
+from typing import Dict, List
+
+from mongoengine import Document, IntField, ReferenceField, StringField, DoesNotExist, EmbeddedDocumentListField, \
+    EmbeddedDocument, DateTimeField
+
+
+class UserInfo(EmbeddedDocument):
+    user_id = IntField(required=True)
+    name = StringField(required=True)
+
+    nickname = StringField(default='')
 
 
 class ChatData(Document):
     chat_id = IntField(required=True)
+    user_infos = EmbeddedDocumentListField(UserInfo) # type: List[UserInfo]
 
     @classmethod
     def get_or_default(cls, chat_id: int):
@@ -13,23 +25,32 @@ class ChatData(Document):
         except DoesNotExist:
             return ChatData(chat_id=chat_id)
 
+    def get_names(self) -> Dict[int, str]:
+        return {
+            user_info.user_id: user_info.name
+            for user_info in self.user_infos
+        }
+
 
 class KarmaUpdate(Document):
     chat = ReferenceField(ChatData, required=True)
     user_id = IntField(required=True)
+    from_id = IntField(required=True)
     update_type = StringField(required=True, choices=['increase', 'decrease'])
 
-    @classmethod
-    def _new_update(cls, chat: ChatData, user_id: int, update_type: str):
-        cls(chat=chat, user_id=user_id, update_type=update_type).save(force_insert=True)
+    update_time = DateTimeField(default=datetime.datetime.now())
 
     @classmethod
-    def give_karma(cls, chat: ChatData, user_id: int):
-        cls._new_update(chat, user_id, 'increase')
+    def _new_update(cls, chat: ChatData, user_id: int, from_id: int, update_type: str):
+        cls(chat=chat, user_id=user_id, from_id=from_id, update_type=update_type).save(force_insert=True)
 
     @classmethod
-    def take_karma(cls, chat: ChatData, user_id: int):
-        cls._new_update(chat, user_id, 'decrease')
+    def give_karma(cls, chat: ChatData, user_id: int, from_id: int):
+        cls._new_update(chat, user_id, from_id, 'increase')
+
+    @classmethod
+    def take_karma(cls, chat: ChatData, user_id: int, from_id: int):
+        cls._new_update(chat, user_id, from_id, 'decrease')
 
     @classmethod
     def get_statistics(cls, chat: ChatData):
@@ -51,7 +72,7 @@ class KarmaUpdate(Document):
                 '_id': '$user_id',
                 'karma': {'$sum': '$update'}
             }},
-            {'$sort':{
+            {'$sort': {
                 'karma': -1
             }}
         )
